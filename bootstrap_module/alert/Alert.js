ab505f50640aee23afe29e9a1d1eae4c68105d26
chenheling <6710582@qq.com>
2016-11-03 14:17:44 +0800

    11.03

--- bootstrap_module/alert/Alert.js
+++ bootstrap_module/alert/Alert.js
@@ -1,32 +1,36 @@
   import React, {
- 	Component,
- 	PropTypes
- } from 'react'
- import ReactDOM from 'react-dom'; 
- import $ from './jquery-vendor.js';
+  	Component,
+  	PropTypes
+  } from 'react'
+  import ReactDOM from 'react-dom';
+  import $ from './jquery-vendor.js';
 
-	var Alert = React.createClass({
-		 
-		render: function() {
-			var me = this;
-			let buttonDom = [];
+  class Alert extends Component {
+  	constructor(props) {
+  	  super(props);
+  	
+  	  this.state = {};
+  	}
+  	render() {
+  		var me = this;
+  		let buttonDom = [];
 
-			if (me.props.prompt) {
+  		if (me.props.prompt) {
 
-				buttonDom.push(<button type="button" className="btn btn-base" onClick={function(){if (me.props.proFun) {me.props.proFun()};}}>确定</button> );
-				buttonDom.push(<button type="button" className="btn btn-default" data-dismiss="modal">取消</button> );
-			}else {
-				buttonDom.push(<button type="button" className="btn btn-default" data-dismiss="modal">确定</button> );
-			}
-			return ( 
-				
-				  <div className="modal-dialog" role="document">
+  			buttonDom.push(<button type="button" className={me.props.okCls} onClick={me.props.proFun}>确定</button>);
+  			buttonDom.push(<button type="button" className={me.props.cancelCls} data-dismiss="modal">取消</button>);
+  		} else {
+  			buttonDom.push(<button type="button" className={me.props.okCls} onClick={me.props.proFun} data-dismiss="modal">确定</button>);
+  		}
+  		return (
+
+  			<div className="modal-dialog" role="document">
 				    <div className="modal-content">
 				      <div className="modal-header">
 				        <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 				        <h4 className="modal-title" id="myModalLabel">{this.props.title}</h4>
 				      </div>
-				      <div className="modal-body" style={{maxHeight:"400px","overflow":"auto"}}>
+				      <div className="modal-body" style={{maxHeight:"400px","overflow":"auto","fontSize":"1.4rem"}}>
 				       {this.props.message}
 				      </div>
 				      <div className="modal-footer">
@@ -34,10 +38,18 @@
 				      </div>
 				    </div>
 				  </div>
-				
-			);
-		}
-	});
 
-	module.exports = Alert;
- 
+  		);
+  	}
+  };
+ Alert.defaultProps = {
+    'prompt': false, //是否是确认提示框
+    proFun:function(){},//确认按钮回调方法
+    'data-nofilesdom': true, //上传的文件是否添加删除展示
+    'title':'消息',//提示框 title
+    message:'',//提示框 文本消息
+    okCls:'btn btn-primary',//ok按钮样式
+    cancelCls:'btn btn-base',//cancel按钮样式
+     
+  };
+  module.exports = Alert;
